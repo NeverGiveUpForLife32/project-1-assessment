fcd3009d4a636f17dad293c09045ff1700a1cbe5
Christopher Fleming <clflem687@yahoo.com>
2023-12-12 20:05:36 -0500

    project-assessment-1

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const plus = querySelector(".plus");
+const minus = querySelector(".minus");
+const input = querySelector(".quantity");
+
+plus.addEventListener("click", (e))
+minus.addEventListener("click", (e))
+
+
+for(let //I WISH I HAD MORE TIME I LITERALLY JUST GOT TO THIS PART!!!!!)
